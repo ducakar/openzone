2d1a8d20c1c2f634181d05611599a3af1b22b124
Davorin Učakar <davorin.ucakar@gmail.com>
2012-02-13 17:42:28 +0100

    add_pch++, build dir can be == source dir
    - add_pch can now work if source dir == build dir
    - cmake files fixed so one can now build in source dir (not a nice practice
      though)

--- src/client/CMakeLists-gen.sh
+++ src/client/CMakeLists-gen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cat << EOF > CMakeLists.txt
-add_library( client STATIC `echo *.cc ui/*.cc` )
+add_library( client STATIC `echo *.cc ui/*.cc | sed 's/openzone\.cc//'` )
 add_dependencies( client pch )
 
-add_executable( openzone openzone/openzone.cc )
+add_executable( openzone openzone.cc )
 add_dependencies( openzone pch )
 target_link_libraries( openzone client modules nirvana matrix common build_info oz \${libs_client} )
 
